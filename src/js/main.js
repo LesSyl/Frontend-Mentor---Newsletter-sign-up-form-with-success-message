ef168fa532ebb9f243c514730fb4ca0b75cbab2f
LesSyl <127938956+LesSyl@users.noreply.github.com>
2024-10-25 10:20:57 +0200

    page added

---
+++ src/js/main.js
@@ -0,0 +1,44 @@
+const inputEmail = document.querySelector('#email')
+const subscribeBtn = document.querySelector('.subscribe')
+const popupBtn = document.querySelector('.popup__button')
+const popup = document.querySelector('.popup')
+const error = document.querySelector('.error')
+const formBoxInput = document.querySelector('.form-box__input')
+const card = document.querySelector('.card')
+
+const clearError = () => {
+	error.classList.add('hidden')
+	formBoxInput.classList.remove('input-error')
+}
+
+const showError = () => {
+	error.classList.remove('hidden')
+	formBoxInput.classList.add('input-error')
+}
+
+const checkMail = email => {
+	const re =
+		/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
+
+	if (re.test(inputEmail.value)) {
+		clearError()
+        showPopup()
+	} else {
+		showError()
+	}
+}
+
+const showPopup = () => {
+	popup.classList.add('popup-active')
+	card.style.visibility='hidden'
+}
+const closePopup = () => {
+    popup.classList.remove('popup-active')
+	card.style.visibility='visible'
+}
+subscribeBtn.addEventListener('click', e => {
+	e.preventDefault()
+	checkMail(email)
+
+})
+popupBtn.addEventListener('click', closePopup)
